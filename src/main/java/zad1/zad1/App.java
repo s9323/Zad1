4bb624cbfd99bb75a747ee83a4ce852526b2c072
student <student@student-VirtualBox>
2012-11-28 09:10:19 +0100

    zad1

---
+++ src/main/java/zad1/zad1/App.java
@@ -0,0 +1,13 @@
+package zad1.zad1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
